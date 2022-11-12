b29d2d5e313dc581504576ebba15031eb786eaa9
mrbitcoiner <mrbitcoiner@tutanota.com>
2022-11-12 00:02:07 +0000

    Working

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Rust Key Stretcher
+## Tool created with the goal of transforming a weak key in a strong key, through expensive CPU and memory process.
+## DISCLAIMER: I'm not a cryptographer, use it at your own risk.
+
+
+### The logic behind the process is basically the following:
+* Hash the input using SHA-512
+* Append the hash bytes into a vector (acumulator) with defined max size
+* Loop until the acumulator is completely filled
+    * Hash the acumulator and append the hash bytes
+    * Duplicate the bytes of the acumulator to increase memory usage
+* Hash the acumulator
+* Hash the hash of the acumulator 100 times, resulting in the final stretched key
+
+## This process is slow, this is good, brute force attempts will run slowly too.
+
+### Build the source:
+````
+cargo build --release
+````
+
+### Build the source:
+````
+cargo build --release
+````
+
+### Run the tool:
+````
+./target/release/key_stretcher
+````
+
+### Run the tool with debug enabled:
+````
+RUST_LOG=debug ./target/release/key_stretcher
+````
---
+++ src/main.rs
@@ -0,0 +1,138 @@
+/* 
+    Rust Key Stretcher
+    By Mr. Bitcoiner
+*/
+
+use hex::encode;
+use sha2::{Sha512, Digest};
+use std::{io, env, process::exit};
+use log::{debug, info};
+use env_logger;
+
+fn do_hash(input: impl AsRef<[u8]>) -> [u8; 64] {
+    let mut hasher = Sha512::new();
+    hasher.update(&input);
+    let hash = hasher.finalize();
+    let mut hash_array: [u8; 64] = [0; 64];
+    hash_array.copy_from_slice(&hash);
+    return hash_array;
+}
+
+fn stretcher(input: &str, max_array_bytes: usize) {
+
+    let input_hash: [u8; 64] = do_hash(&input);
+    let mut acumulator: Vec<u8> = Vec::with_capacity(max_array_bytes);
+
+    /* Append first key hash to the acumulator */
+    for i in 0..input_hash.len(){
+        acumulator.push(input_hash[i]);
+    }
+
+    let mut acumulator_length:usize = 0;
+    let mut acumulator_round:usize = 0;
+    while acumulator_length < max_array_bytes {
+        /* Append the hash of acumulator to the acumulator */
+        let new_round: [u8; 64] = do_hash(&acumulator);
+        for i in 0..new_round.len() {
+            acumulator.push(new_round[i]);
+        }
+
+        /* Increase the acumulator length */
+        let actual_acumulator_length:usize = acumulator.len();
+        for i in 0..actual_acumulator_length{
+            if acumulator.len() < max_array_bytes{
+                acumulator.push(acumulator[i]);
+            }
+            else {
+                break;
+            }
+        }
+
+        acumulator_round += 1;
+        acumulator_length = acumulator.len();
+        debug!("Key stretch round {}, acumulator size: {}MB.", acumulator_round, acumulator.len()/1024/1024);
+    }
+    let mut final_hash: [u8; 64] = do_hash(&acumulator);
+    for i in 0..100{
+        final_hash = do_hash(&final_hash);
+        debug!("Hash Round: {}, hash: {}",i+1, encode(&final_hash));
+    }
+    print!("stretched_key={}", encode(&final_hash).as_str());
+}
+
+fn set_mode(input: String) -> String {
+    match input.trim() {
+        "low"=> {
+            return String::from("low");
+        }
+        "med"=>{
+            return String::from("med");
+        }
+        "high"=>{
+            return String::from("high");
+        }
+        "extreme"=>{
+            return String::from("extreme");
+        }
+        _=>{
+            print!("Memory usage modes: [ low: 512MB | mid: 1GB | high: 2GB | extreme: 4GB ]\n");
+            exit(1);
+        }
+    }
+}
+
+fn set_logger() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "error");
+    }
+    env_logger::init();
+}
+
+fn main() {
+    set_logger();
+
+    let low:usize =  536870912; // 512MB
+    let med:usize = 1073741824; // 1GB
+    let high:usize = 2147483648; // 2GB
+    let extreme:usize = 4294967296; // 4GB
+
+    let mut mode:String = String::new();
+    let mut key:String = String::new();
+
+    let args: Vec<String> = env::args().collect();
+    match env::args().len() {
+        2 => {
+            mode = set_mode(args[1].to_string());
+            print!("\nInsert the key: \n");
+            io::stdin().read_line(&mut key).expect("Failed to read input");
+        }
+        3 => {
+            mode = set_mode(args[1].to_string());
+            key = args[2].to_string();
+        }
+        _ => {
+            print!("Expected args:\n\t mandatory: <memory_usage_mode> optional: <key>\n");
+            if mode.is_empty() { return; }
+        }
+    }
+
+    info!("Mode: {}, key: {}", mode.as_str(), key.trim());
+
+    match mode.as_str() {
+        "low"=> {
+            stretcher(key.trim(), low);
+        }
+        "med"=>{
+            stretcher(key.trim(), med);
+        }
+        "high"=>{
+            stretcher(key.trim(), high);
+        }
+        "extreme"=>{
+            stretcher(key.trim(), extreme);
+        }
+        _=>{
+            exit(1);
+        }
+    }
+}
